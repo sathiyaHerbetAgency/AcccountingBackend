3debe0273fbdecb08ae8548378329a3597e8293f
sathiya <sathiya@herbet.agency>
2025-03-24 16:08:41 +0530

    edited

--- Client Side/db/db.js
+++ Client Side/db/db.js
@@ -1,7 +1,8 @@
 import {createConnection } from 'mongoose';
 // const url="mongodb+srv://<sathiya>:<sathiya@cluster0.ehydi.mongodb.net/";
+const dbUri = 'mongodb+srv://sathiya:sathiya@cluster0.ehydi.mongodb.net/' || 'mongodb://0.0.0.0:27017/InvoiceManager';
 
-var con1=createConnection('mongodb://0.0.0.0:27017/InvoiceManager',
+var con1=createConnection(dbUri,
   {
     useNewUrlParser: true,
 
